ee98728f466cd1bdd5945453e7d193f8a36b16d9
Remy <remy.auricoste@gmail.com>
2014-03-06 21:03:09 +0100

    add limit tests to get function

--- test/db/SyncDbSpec.js
+++ test/db/SyncDbSpec.js
@@ -49,7 +49,6 @@ define([
                     .then(function() {return db.get({_id: object._id})}).then(function(result) {expect(result).toEqual(object)})
                     .then(function() {return db.get({_id: object._id, _rev: object2._rev})}).then(function(result) {expect(result).toEqual(object)})
                     .then(function() {
-                        console.log("OK !");
                         testOk = true;
                     }).fail(log);
                 waitsFor(asyncTest);
@@ -61,8 +60,6 @@ define([
                         var object2 = $.extend({}, object);
                         object2.value = "test2";
                         db.save(object2).then(function (object2) {
-                            console.log(object);
-                            console.log(object2);
                             expect(object).not.toEqual(object2);
                             Q.all([
                                 db.get(object2),
@@ -78,6 +75,27 @@ define([
                             });
                         });
                     }).fail(log);
+                waitsFor(asyncTest);
+            });
+
+            it('throw an exception if get is called without _id field', function () {
+                try {
+                db.get({}).then(function() {
+                    expect("an expection to throwned").toBe(true);
+                }).fail(function() {
+                    testOk = true;
+                });
+                }catch(e) {
+                    testOk = true;
+                }
+                waitsFor(asyncTest);
+            });
+
+            it('get function return null if no doc is found', function () {
+                db.get({_id:"plop"}).then(function(result) {
+                    expect(result).toBe(null);
+                    testOk = true;
+                });
                 waitsFor(asyncTest);
             });
         })
