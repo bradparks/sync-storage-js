34c6c84aed2ef577a2774ae4075a57ff148a13d0
Remy <remy.auricoste@gmail.com>
2014-03-06 20:37:42 +0100

    fix test no2

--- test/db/SyncDbSpec.js
+++ test/db/SyncDbSpec.js
@@ -14,16 +14,6 @@ define([
                 console.log("error");
                 console.log(object);
             }
-            var expectPromiseEqual = function(promise, object) {
-                var testOk = false;
-                promise.then(function(result) {
-                    expect(result).toEqual(object);
-                    testOk = true;
-                }).fail(log);
-                waitsFor(function() {
-                    return testOk;
-                });
-            }
 
             beforeEach(function () {
                 db = new SyncDB("local");
@@ -45,15 +35,19 @@ define([
             });
 
             it('finds an object from its _id or its _id and _rev', function () {
+                var object2;
                 db.save(object)
                     .then(function(result) {
                         object = result;
                         return db.get(result);
                     })
                     .then(function (result) {
+                        object2 = result;
                         expect(result).toEqual(object);
-                        expectPromiseEqual(db.get({_id: object._id}), object);
-                        expectPromiseEqual(db.get({_id: object._id, _rev: result._rev}), object);
+                    })
+                    .then(function() {return db.get({_id: object._id})}).then(function(result) {expect(result).toEqual(object)})
+                      .then(function() {return db.get({_id: object._id, _rev: object2._rev})}).then(function(result) {expect(result).toEqual(object)})
+                      .then(function() {
                         console.log("OK !");
                         testOk = true;
                     }).fail(log);
