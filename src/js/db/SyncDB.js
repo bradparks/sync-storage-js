092df58103e1939067dfb505c9e08e7debea7dc5
remy <remy.auricoste@gmail.com>
2014-03-06 15:03:03 +0100

    using promises...

--- src/js/db/SyncDB.js
+++ src/js/db/SyncDB.js
@@ -6,45 +6,67 @@ define([
     "q",
     "db/InMemoryStorage"
 ],
-function (PouchDB, $, StringUtils, _, Q, Storage) {
-    console.log("loading SyncDB");
+    function (PouchDB, $, StringUtils, _, Q, Storage) {
+        console.log("loading SyncDB");
 
-    var classe = function (url) {
-        this.name = url;
-        this.isLocal = !StringUtils.startsWith(url, "http");
-        this.storage = new Storage(url);
-    }
+        var classe = function (url) {
+            this.name = url;
+            this.isLocal = !StringUtils.startsWith(url, "http");
+            this.storage = new Storage(url);
+        };
 
-    var generateHash = function() {
-        var dic = "0987654321azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
-        var hash = "";
-        for (var i=0;i<30;i++) {
-            hash += dic.charAt(_.random(0, dic.length-1));
-        }
-    }
+        var generateHash = function () {
+            var dic = "0987654321azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+            var hash = "";
+            for (var i = 0; i < 30; i++) {
+                hash += dic.charAt(_.random(0, dic.length - 1));
+            }
+            return hash;
+        };
 
-    classe.prototype.save = function(object) {
-        var resultObject = $.extend({}, object);
-        if (!resultObject._id) {
-            resultObject._id = new Date().getTime() + "";
-        }
-        var version = 1;
-        if (resultObject._rev) {
-            var rev = resultObject._rev;
-            var version = rev.substring(0, rev.indexOf('-'));
-            version++;
-        }
-        resultObject._rev = version+"-"+generateHash();
-        this.storage.save(resultObject);
-        return resultObject;
-    }
+        var parseRev = function (revString) {
+            var split = revString.split('-');
+            return {
+                version: split[0],
+                hash: split[1]
+            }
+        };
 
-    classe.prototype.get = function(query) {
-        if (!query._id) {
-            throw "get method : _id must be filled";
-        }
-        return this.storage.get(query);
-    }
+        classe.prototype.save = function (object) {
+            var self = this;
+            var resultObject = $.extend({}, object);
+            if (!resultObject._id) {
+                resultObject._id = new Date().getTime() + "";
+            }
+            var version = 1;
+            if (resultObject._rev) {
+                var version = parseRev(resultObject._rev).version;
+                version++;
+            }
+            resultObject._rev = version + "-" + generateHash();
+            var combinedKey = resultObject._id + "/" + resultObject._rev;
+            self.storage.save(combinedKey, resultObject);
+            // TODO put lock on write
+            return self.storage.get(resultObject._id).then(function (lastObject) {
+                console.log("last=" + JSON.stringify(lastObject));
+                if (!lastObject || parseRev(lastObject._rev).version < version) {
+                    return self.storage.save(resultObject._id, resultObject);
+                }
+                return resultObject;
+            });
+        };
 
-    return classe;
-});
+        classe.prototype.get = function (query) {
+            if (!query._id) {
+                throw "get method : _id must be filled";
+            }
+            var key = query._id;
+            if (!query._rev) {
+                key += "/" + query._rev;
+            }
+            console.log("looking for "+key);
+            return this.storage.get(key);
+        };
+
+        return classe;
+    });
--- test/db/SyncDbSpec.js
+++ test/db/SyncDbSpec.js
@@ -3,41 +3,79 @@ define([
     "jquery"
 ],
     function (SyncDB, $) {
-    describe('SyncDB', function () {
-        var db;
-        var object;
+        describe('SyncDB', function () {
+            var db;
+            var object;
+            var testOk;
+            var asyncTest = function () {
+                return testOk;
+            }
+            var log = function (object) {
+                console.log("error");
+                console.log(object);
+            }
+            var expectPromiseEqual = function(promise, object) {
+                var testOk = false;
+                promise.then(function(result) {
+                    expect(result).toEqual(object);
+                    testOk = true;
+                }).fail(log);
+                waitsFor(function() {
+                    return testOk;
+                });
+            }
 
-        beforeEach(function () {
-            db = new SyncDB("local");
-            object = {value:"test"};
-        });
+            beforeEach(function () {
+                db = new SyncDB("local");
+                object = {value: "test"};
+                testOk = false;
+            });
 
-        it('stores an object and return an object with _id and _rev fields', function () {
-            object = db.save(object);
-            expect(object._id).not.toBe(undefined);
-            expect(object._rev).not.toBe(undefined);
-            expect(object._rev).not.toBe(undefined);
-            expect(object._rev).toMatch(/[0-9]+-[0-9a-zA-Z]+/);
-            expect(object.value).toBe("test");
-        });
+            it('stores an object and return an object with _id and _rev fields', function () {
 
-        it('finds an object from its _id or its _id and _rev', function () {
-            object = db.save(object);
-            expect(db.get(object)).toEqual(object);
-            expect(db.get({_id:object._id})).toEqual(object);
-            expect(db.get({_id:object._id, _rev:object._rev})).toEqual(object);
-        });
+                db.save(object).then(function (object) {
+                    expect(object._id).not.toBe(undefined);
+                    expect(object._rev).not.toBe(undefined);
+                    expect(object._rev).not.toBe(undefined);
+                    expect(object._rev).toMatch(/[0-9]+-[0-9a-zA-Z]+/);
+                    expect(object.value).toBe("test");
+                    testOk = true;
+                }).fail(log);
+                waitsFor(asyncTest);
+            });
 
-        it('finds an object from its _id or its _id and _rev (2 versions)', function () {
-            object = db.save(object);
-            var object2 = $.extend({}, object);
-            object2.value = "test2";
-            object2 = db.save(object2);
-            expect(object).not.toEqual(object2);
-            expect(db.get(object2)).toEqual(object2);
-            expect(db.get({_id:object._id})).toEqual(object2);
-            expect(db.get({_id:object._id, _rev:object._rev})).toEqual(object);
-            expect(db.get({_id:object._id, _rev:object2._rev})).toEqual(object2);
-        });
-    })
-});
+            it('finds an object from its _id or its _id and _rev', function () {
+                db.save(object)
+                    .then(function(result) {
+                        object = result;
+                        return db.get(result);
+                    })
+                    .then(function (result) {
+                        expect(result).toEqual(object);
+                        expectPromiseEqual(db.get({_id: object._id}), object);
+                        expectPromiseEqual(db.get({_id: object._id, _rev: result._rev}), object);
+                        console.log("OK !");
+                        testOk = true;
+                    }).fail(log);
+                waitsFor(asyncTest);
+            });
+
+            it('finds an object from its _id or its _id and _rev (2 versions)', function () {
+                db.save(object)
+                    .then(function (object) {
+                        var object2 = $.extend({}, object);
+                        object2.value = "test2";
+                        db.save(object2).then(function (object2) {
+                            expect(object).not.toEqual(object2);
+
+                            expectPromiseEqual(db.get(object2), object2);
+                            expectPromiseEqual(db.get({_id: object._id}), object2);
+                            expectPromiseEqual(db.get({_id: object._id, _rev: object._rev}), object);
+                            expectPromiseEqual(db.get({_id: object._id, _rev: object2._rev}), object2);
+                            testOk = true;
+                        });
+                    }).fail(log);
+                waitsFor(asyncTest);
+            });
+        })
+    });
