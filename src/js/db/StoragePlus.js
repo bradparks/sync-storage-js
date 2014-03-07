061362a765bbaf33c9479fd64b63040d001e5046
Remy <remy.auricoste@gmail.com>
2014-03-07 12:20:35 +0100

    add StoragePlusSpec

--- src/js/db/StoragePlus.js
+++ src/js/db/StoragePlus.js
@@ -3,6 +3,7 @@ define([
     var classe = function(name, storage) {
         this.name = name;
         this.storage = storage;
+        this.indexStorage = null;
     }
 
     var getNewKey = function(self, key) {
@@ -19,7 +20,31 @@ define([
 
     classe.prototype.query = function(query) {
         // TODO create index to fasten search
-    };
+        var defer = Q.defer();
+        var rows = {};
+        var total = 0;
+        var emit = function(key, value) {
+            var array = rows[key];
+            if (!array) {
+                array = [];
+                rows[key] = array;
+            }
+            array.push(value);
+            total++;
+        }
+        console.log(JSON.stringify(this, null, 2));
+        for (var key in this) {
+            var doc = this[key];
+            if (typeof doc != 'function') {
+                query.mapFunction(emit)(doc);
+            }
+        }
+        defer.resolve({
+            total_rows:total,
+            rows:rows
+        });
+        return defer.promise;
+    }
 
     return classe;
 });
---
+++ test/db/StoragePlusSpec.js
@@ -0,0 +1,32 @@
+define([
+    "db/StoragePlus",
+    "db/InMemoryStorage"
+],
+    function (StoragePlus, Storage) {
+        describe('StoragePlus', function () {
+            var testOk;
+            var storage;
+            var object;
+            var asyncTest = function () {
+                return testOk;
+            }
+
+            beforeEach(function () {
+                storage = new StoragePlus("storage", new Storage());
+                object = {value: "test"};
+                testOk = false;
+            });
+
+            it('stores an object and retrieve an object', function () {
+                var key = "clef";
+                var objectOrigin = object;
+                storage.save(key, objectOrigin).then(function () {
+                    return storage.get(key);
+                }).then(function (object) {
+                    expect(object).toBe(objectOrigin);
+                    testOk = true;
+                });
+                waitsFor(asyncTest);
+            });
+        })
+    });
