5db8366ed7590c2060e54d3aa7d60e80d9dd7dff
Remy <remy.auricoste@gmail.com>
2014-03-11 13:19:39 +0100

    IndexStorage : fix race condition on update indexes

--- src/js/db/IndexStorage.js
+++ src/js/db/IndexStorage.js
@@ -1,26 +1,43 @@
 define([
-], function() {
+"underscore"
+], function(_) {
     var classe = function(storage) {
         this.storage = storage;
+        this.buffer = {};
     }
-    
-    classe.prototype.addIndexKey = function(key, value) {
-        var self = this;
+
+    var checkBuffer = function(self) {
+        if (_.size(self.buffer) == 0) {
+            return;
+        }
         return self.getAll().then(function(result) {
             var array = result;
             if (!array) {
                 array = {};
             }
-            console.log("adding to index : "+key);
+            var key;
+            var value;
+            for (var key2 in self.buffer) {
+                key = key2;
+                value = self.buffer[key];
+                break;
+            }
+            delete self.buffer[key];
             array[key] = value;
             return self.storage.save("_all", array);
+        }).then(function() {
+            return checkBuffer(self);
         });
+    }
+    
+    classe.prototype.addIndexKey = function(key, value) {
+        this.buffer[key] = value;
+        return checkBuffer(this);
     }
 
     classe.prototype.getAll = function() {
-        var self = this;
-        return self.storage.get("_all").then(function(result) {
-            return result ? result : [];
+        return this.storage.get("_all").then(function(result) {
+            return result ? result : {};
         });
     }
     
