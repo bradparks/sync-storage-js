c3511c7bba87541110f304bf22b2cbb82ca35393
Remy <remy.auricoste@gmail.com>
2014-03-16 11:11:41 +0100

    add test destroy method on browser

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,21 @@
 define([
 "db/SyncDB",
 "basicStorage/LocalForageBridge",
+"basicStorage/FacadeStorage",
 "utils/FunctionUtils",
 "localForage",
 "q"
-], function(SyncDB, LocalForageBridge, FunctionUtils, localForage, Q) {
+], function(SyncDB, LocalForageBridge, FacadeStorage, FunctionUtils, localForage, Q) {
     return function() {
         // hack to fix localForage init bug
         // https://github.com/mozilla/localForage/issues/65
         FunctionUtils.onCondition(function() {
             return localForage.driver ? true : false;
         }, function() {
-            var db = new SyncDB("test", new LocalForageBridge());
-            db.save({
-                value:"plop"
-            });
+            var db = new SyncDB("test", new FacadeStorage());
             var input = new Date().getTime();
             var promises = [];
-            for (var i=0;i<50;i++) {
+            for (var i=0;i<5;i++) {
                 var promise = db.save({
                     value:"plop"
                 });
@@ -38,6 +36,8 @@ define([
                     var endQuery = new Date().getTime();
                     console.log("elapsed time = "+(endQuery - startQuery));
                     console.log(result);
+                }).then(function() {
+                    db.destroy();
                 });
             });
 
