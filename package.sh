8fda6ac759fc24c30e0564dbf5cc46559f5e7207
Remy <remy.auricoste@gmail.com>
2014-03-17 23:42:08 +0100

    add FacadeStorage in the packaging

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+node node_modules/requirejs/bin/r.js -o require-build.js optimize=none out=dist/FacadeStorage.js name=basicStorage/FacadeStorage
 node node_modules/requirejs/bin/r.js -o require-build.js
 node node_modules/requirejs/bin/r.js -o require-build.js optimize=none out=dist/SyncStorage.js
