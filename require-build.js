b3ffda45bf650f1b31ac37115115b112e38d84e8
Remy <remy.auricoste@gmail.com>
2014-03-17 23:12:18 +0100

    add requirejs builder

---
+++ require-build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: "src/js",
+    name: "SyncStorage",
+    out: "dist/SyncStorage.js",
+    mainConfigFile: 'src/js/main.js',
+    paths: {
+        "q":"empty:",
+        "underscore":"empty:",
+        "localForage":"empty:"
+    }
+})
