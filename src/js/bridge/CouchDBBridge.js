6ea7def16933d7ecc3630a0cf5d442e6c7de9bb7
Remy <remy.auricoste@gmail.com>
2014-03-22 19:23:57 +0100

    add CouchDB bridge

---
+++ src/js/bridge/CouchDBBridge.js
@@ -0,0 +1,91 @@
+define([
+"utils/Request"
+], function(Request) {
+    var classe = function(host, collectionName) {
+        var self = this;
+        this.host = host;
+        this.name = collectionName;
+        this.url = host + "/" + this.name + "/";
+    }
+
+    classe.prototype.exists = function() {
+        return new Request("get", this.url).call().then(function(result) {
+            return result.statusCode == 200;
+        });
+    }
+
+    classe.prototype.create = function() {
+        return new Request("put", this.url).call();
+    }
+
+    classe.prototype.isSupported = function() {
+        return new Request("get", this.host).call().then(function(result) {
+            try {
+                var data = JSON.parse(result.data)
+                return data.couchdb ? true : false;
+            } catch(e) {
+                return false;
+            }
+        }).fail(function() {
+            return false;
+        });
+    }
+
+    var transformKey = function(key) {
+        var result = key.toLowerCase().replace("/", "$");
+        return result;
+    }
+
+    var getCouchObject = function(self, key) {
+        return new Request("get", self.url+transformKey(key)).call();
+    }
+
+    classe.prototype.save = function(key, object) {
+        var self = this;
+        return getCouchObject(self, key).then(function(result) {
+            var storedObject = {
+                data:JSON.stringify(object)
+            }
+            if (result && result.data) {
+                var couchObject = JSON.parse(result.data);
+                if (couchObject._rev) {
+                    storedObject._rev = couchObject._rev;
+                }
+            }
+            console.log(storedObject);
+            return new Request("put", self.url+transformKey(key), JSON.stringify(storedObject)).call().then(function() {
+                return object;
+            });
+        });
+    }
+
+    classe.prototype.get = function(key) {
+        return new Request("get", this.url+transformKey(key)).call().then(function(result) {
+            return result.data ? JSON.parse(JSON.parse(result.data).data) : null;
+        });
+    }
+
+    classe.prototype.del = function(key) {
+        var self = this;
+        return getCouchObject(self, key).then(function(result) {
+            if (result && result.data) {
+                var couchObject = JSON.parse(result.data);
+                if (couchObject._rev) {
+                    return new Request("delete", self.url+transformKey(key)+"?rev="+couchObject._rev).call();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }).fail(function() {
+            return false;
+        });
+    }
+
+    classe.prototype.destroy = function() {
+        return new Request("delete", this.url).call();
+    }
+
+    return classe;
+});
---
+++ test/bridge/RemoteBridgeSpec.js
@@ -0,0 +1,83 @@
+define([
+    "bridge/CouchDBBridge"
+],
+function (Bridge) {
+    describe('Bridge', function () {
+
+        var waitPromise = function(promise) {
+            var resolved = false;
+            promise.then(function() {
+                resolved = true;
+            }).fail(function(err) {
+                console.error(JSON.stringify(err));
+            });
+            waitsFor(function() {
+                return resolved;
+            });
+        };
+        var log = function(object) {
+            console.log(JSON.stringify(object));
+        };
+        var object;
+
+        beforeEach(function () {
+            storage = new Bridge("http://localhost:5984", "test");
+            waitPromise(storage.destroy());
+            object = {value: "test"};
+        });
+
+        it('isSupported should return true', function () {
+            waitPromise(
+                storage.isSupported().then(function(result) {
+                    expect(result).toBe(true);
+                })
+            );
+        });
+
+        it('isSupported should return false', function () {
+            storage = new Bridge("http://localhost:1000", "test");
+            waitPromise(
+                storage.isSupported().then(function(result) {
+                    expect(result).toBe(false);
+                })
+            );
+        });
+
+        it('exists should return false', function () {
+            waitPromise(
+                storage.exists().then(function(result) {
+                    expect(result).toBe(false);
+                })
+            );
+        });
+
+        it('exists should return true', function () {
+            waitPromise(
+                storage.create().then(function() {
+                    return storage.exists();
+                }).then(function(result) {
+                    expect(result).toBe(true);
+                })
+            );
+        });
+
+        it('should store key/value and then delete it', function () {
+            waitPromise(
+                storage.create().then(function() {
+                    return storage.save("key", object);
+                }).then(function() {
+                    return storage.get("key");
+                }).then(function(result) {
+                    expect(result).toEqual(object);
+                }).then(function() {
+                    return storage.del("key");
+                }).then(function() {
+                    return storage.get("key");
+                }).then(function(result) {
+                    expect(result).toBe(null);
+                })
+            );
+        });
+
+    });
+});
