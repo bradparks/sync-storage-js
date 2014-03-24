d6954d470585fa889884fb32915fcece9e4be892
Remy <remy.auricoste@gmail.com>
2014-03-24 19:29:09 +0100

    add Lock object, with tests

---
+++ src/js/utils/Lock.js
@@ -0,0 +1,50 @@
+define([
+"q",
+"utils/FunctionUtils",
+"utils/Logger"
+], function(Q, FunctionUtils, Logger) {
+    var classe = function(resources) {
+        this.resources = 1;
+        if (resources) {
+            this.resources = resources;
+        }
+    }
+
+    var logger = new Logger("Lock", Logger.INFO);
+
+    var isPromise = function(object) {
+        return object.then ? true : false;
+    }
+
+    classe.prototype.synchronize = function() {
+        var defer = Q.defer();
+        var self = this;
+        logger.debug("enter synchronize");
+        FunctionUtils.onCondition(function() {
+            var pass = false;
+            logger.debug("check condition : "+self.resources);
+            if (self.resources >= 1) {
+                logger.debug("taking lock");
+                self.resources--;
+                logger.debug("resources : "+self.resources);
+                pass = true;
+            }
+            return pass;
+        }, function() {
+            logger.debug("resolving promise");
+            defer.resolve();
+        });
+        return defer.promise;
+    }
+
+    classe.prototype.release = function() {
+        var self = this;
+        logger.debug("release call");
+        return Q.fcall(function() {
+             self.resources += 1;
+             logger.debug("releasing lock : "+self.resources);
+        });
+    }
+
+    return classe;
+});
---
+++ test/utils/LockSpec.js
@@ -0,0 +1,62 @@
+define([
+    "utils/Lock",
+    "q",
+    "utils/Logger"
+],
+function (Lock, Q, Logger) {
+    describe('Lock', function () {
+        var logger = new Logger("LockSpec");
+        var testOk;
+        var log = function(object) {
+            logger.info(JSON.stringify(object));
+        }
+        var asyncTest = function() {
+            return testOk;
+        }
+
+        beforeEach(function () {
+            logger.info("start test");
+            testOk = false;
+        });
+
+        it('init with number of resources', function () {
+            var lock = new Lock();
+            expect(lock.resources).toBe(1);
+            lock = new Lock(6);
+            expect(lock.resources).toBe(6);
+        });
+
+        it('authorize only one function at a time', function () {
+            var lock = new Lock();
+            var value = 0;
+            var promise1 = lock.synchronize().then(function() {
+                value++;
+            }).then(function() {
+                expect(value).toBe(1);
+            }).then(function() {
+                value--;
+            }).then(function() {
+                expect(value).toBe(0);
+            }).then(function() {
+                return lock.release();
+            });
+
+            var promise2 = lock.synchronize().then(function() {
+                value++;
+            }).then(function() {
+                expect(value).toBe(1);
+            }).then(function() {
+                value--;
+            }).then(function() {
+                expect(value).toBe(0);
+            }).then(function() {
+                return lock.release();
+            });
+
+            Q.all([promise1, promise2]).then(function() {
+                testOk = true;
+            });
+            waitsFor(asyncTest);
+        });
+    });
+});
